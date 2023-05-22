82633e9566aa020abf29656159f6891bd64658b1
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-22 23:19:42 +0300

    Initial Commit

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,11 @@
 import java.io.Serializable;
 
 public class Process implements Serializable {
+    private final String processName;
+    private final PCB processControlBlock;
+
+    public Process(String processName, PCB processControlBlock) {
+        this.processName = processName;
+        this.processControlBlock = processControlBlock;
+    }
 }
