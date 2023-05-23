8b2359fdea570a46ce80f02969248e0aed9a5150
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-23 11:08:20 +0300

    Initial Commit

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,2 +1,38 @@
 public class Interpreter {
+
+    public static void print(String s){
+        System.out.println(s);
+    }
+
+    public static void semWait(String resourceType){
+        switch(resourceType){
+            case "file" ->{
+                if(!Kernel.fileMutex.tryAcquire())
+                    return;
+            }
+            case "userInput" -> {
+                if(!Kernel.inputMutex.tryAcquire())
+                    return;
+            }
+            case "userOutput" -> {
+                if(!Kernel.outputMutex.tryAcquire())
+                    return;
+            }
+        }
+    }
+
+    public static void semSignal(String resourceType){
+        switch(resourceType){
+            case "file" ->{
+                Kernel.fileMutex.release();
+            }
+            case "userInput" ->{
+                Kernel.inputMutex.release();
+            }
+            case "userOutput" ->{
+                Kernel.outputMutex.release();
+            }
+        }
+    }
+
 }
