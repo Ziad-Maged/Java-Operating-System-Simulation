a02e80e89db64db05eb9e366e939e0a3fc0cddc1
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-23 10:55:31 +0300

    Initial Commit

--- src/Kernel.java
+++ src/Kernel.java
@@ -1,9 +1,18 @@
+import java.util.concurrent.Semaphore;
+
 public class Kernel {
 
     static Object[] memory = new Object[40];
 
-    public static void main(String[] args) {
-        //TODO LATER
+    public static void main(String[] args) throws InterruptedException {
+        int time = 0;
+        Semaphore s = new Semaphore(1);
+        boolean running = true;
+        while (running){
+            //TODO LATER
+            time++;
+            running = false;
+        }
     }
 
 }
