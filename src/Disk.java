a9c8a2227c768bd7701536b0f8a398655b760e6d
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-26 19:34:19 +0300

    Initial Commit

--- src/Disk.java
+++ src/Disk.java
@@ -1,4 +1,25 @@
-import java.io.Serializable;
+import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class Disk implements Serializable {
+
+    ArrayList<Process> processesOnDisk;
+
+    public Disk(){
+        processesOnDisk = new ArrayList<>();
+    }
+
+    public void saveDisk(){
+        try {
+            FileOutputStream fileOut = new FileOutputStream("Disk.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
