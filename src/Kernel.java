c16dac58027280c43c537ee66fcb90722318cd07
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-26 21:32:42 +0300

    Initial Commit

--- src/Kernel.java
+++ src/Kernel.java
@@ -2,19 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Semaphore;
 
 public class Kernel {
 
     static Object[] memory = new Object[40];
-    static int instructionsPointerInMemory = memory.length;
     static int currentMemorySpace = memory.length;
-    static int pcbPlaceholder = 0;
+    static int placeholder = 0;
     static Semaphore fileMutex = new Semaphore(1);
     static Semaphore userInputMutex = new Semaphore(1);
     static Semaphore userOutputMutex = new Semaphore(1);
     static ArrayList<Process> processes = new ArrayList<>();
-    static ArrayList<Integer> processPCBIndices = new ArrayList<>();
     static Disk disk = new Disk();
     public static void startProgram(){
         //TODO Later
@@ -30,47 +29,39 @@ public class Kernel {
                 decodeInstruction(s, fullInstruction, instructions);
             }
             if(instructions.size() + 8 <= currentMemorySpace){
-                int maximumInMemory = instructionsPointerInMemory;
-                for(int i = instructions.size() - 1; i >= 0; i--){
-                    memory[instructionsPointerInMemory--] = instructions.get(i);
+                PCB pcb = new PCB(processID, placeholder + 8, placeholder + 8 + instructions.size());
+                memory[placeholder++] = processID;
+                currentMemorySpace--;
+                memory[placeholder++] = pcb.getProcessState();
+                currentMemorySpace--;
+                memory[placeholder++] = pcb.getProgramCounter();
+                currentMemorySpace--;
+                memory[placeholder++] = pcb.getMinimumInMemory();
+                currentMemorySpace--;
+                memory[placeholder++] = pcb.getMaximumInMemory();
+                currentMemorySpace--;
+                memory[placeholder++] = new Variable();
+                currentMemorySpace--;
+                memory[placeholder++] = new Variable();
+                currentMemorySpace--;
+                memory[placeholder++] = new Variable();
+                currentMemorySpace--;
+                for(Instruction e : instructions){
+                    memory[placeholder++] = e;
                     currentMemorySpace--;
                 }
-                int minimumInMemory = instructionsPointerInMemory;
-                PCB pcb = new PCB(processID, minimumInMemory, maximumInMemory);
-                Process process = new Process("Program_" + processID, pcb, instructions.size(), instructions.size() + 8);
-                for(Instruction e : instructions)
-                    process.getInstructions().add(e);
-                processes.add(process);
-                processPCBIndices.add(pcbPlaceholder);
-                memory[pcbPlaceholder++] = processID;
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = pcb.getProcessState();
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = pcb.getProgramCounter();
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = pcb.getMinimumInMemory();
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = pcb.getMaximumInMemory();
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = new Variable();
-                process.setVar1((Variable) memory[pcbPlaceholder - 1]);
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = new Variable();
-                process.setVar2((Variable) memory[pcbPlaceholder - 1]);
-                currentMemorySpace--;
-                memory[pcbPlaceholder++] = new Variable();
-                process.setVar3((Variable) memory[pcbPlaceholder - 1]);
-                currentMemorySpace--;
             }else {
                 Process p = null;
-                for(int i = 0; i < pcbPlaceholder; i += 8){
+                for(int i = 0; i < placeholder; i += 8){
                     p = processToSwap(i);
                 }
                 assert p != null;
                 p.setInDisk(true);
                 disk.getProcessesOnDisk().add(p);
+                removeProcessFromMemory(p);
                 disk.saveDisk();
                 disk.write();
+                allocateProcessToMemory(processID);
             }
         } catch (IOException ignored) {}
     }
@@ -86,11 +77,74 @@ public class Kernel {
     private static void removeProcessFromMemory(Process p){
         for(int i = 0; i < memory.length; i++){
             if(memory[i] instanceof Integer && ((Integer)memory[i]) == p.getProcessControlBlock().getProcessID()){
-                pcbPlaceholder = i;
+                placeholder = i;
+                break;
+            }
+        }
+        if(placeholder + 8 + p.getInstructions().size() >= memory.length || memory[placeholder + 8 + p.getInstructions().size()] == null){
+            for(int i = placeholder; i < placeholder + 8 + p.getInstructions().size(); i++){
+                memory[i] = null;
+                currentMemorySpace++;
+            }
+        }else if(memory[placeholder + 8 + p.getInstructions().size()] != null){
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+            memory[placeholder + 8 + p.getInstructions().size()] = null;
+            placeholder++;
+            currentMemorySpace++;
+            for(int i = 0; i < p.getInstructions().size(); i++){
+                memory[placeholder] = memory[placeholder + 8 + p.getInstructions().size()];
+                memory[placeholder + 8 + p.getInstructions().size()] = null;
+                placeholder++;
+                currentMemorySpace++;
             }
         }
     }
+    private static void shifting(Process p)
+    {
+        for(int i = 0; i < memory.length; i++){
+            if(memory[i] instanceof Integer && ((Integer)memory[i]) == p.getProcessControlBlock().getProcessID()){
+                placeholder = i;
+                break;
+            }
+        }
+        for(int j =0 ; j< 3; j++){
+            for(int i =placeholder; i<currentMemorySpace; i ++){
 
+                memory[i+1] =memory[i];
+                memory[i] = null;
+            }
+        }
+
+
+    }
     private static void memoryToDisk(){
         //TODO
     }
